69a6c40f49744912c7307b222dabba8e4d107fbe
Ben Cutler <b.cutler724@gmail.com>
2020-09-09 04:17:51 -0400

    Finished setting up the level select menu

---
+++ Assets/Scripts/Utilities/CanvasUtility.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof (Canvas))]
+public class CanvasUtility : MonoBehaviour {
+
+	private Selectable[] selectables;
+
+	private void Awake () {
+		selectables = GetComponentsInChildren<Selectable> ();
+	}
+
+	public void SetRaycastTargetsEnabled (bool enabled) {
+		foreach (Selectable s in selectables) {
+			s.interactable = enabled;
+		}
+	}
+}

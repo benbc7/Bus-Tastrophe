eb3d678b85dabf41853b08109e9a7a272971f6ec
Ben Cutler <b.cutler724@gmail.com>
2020-09-09 23:51:33 -0400

    Set up the start of a pause button

--- Assets/Scripts/Managers/objective/PauseMenuManager.cs
+++ Assets/Scripts/Managers/objective/PauseMenuManager.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseMenuManager : MonoBehaviour
-{
+public class PauseMenuManager : MonoBehaviour {
 
-    public void returnToMainMenu()
-    {
-        print("returning to main menu");
-        SceneManager.LoadScene("mainmenu");
+    public static PauseMenuManager instance;
+
+    public GameObject pauseMenuCanvas;
+
+    private bool isPaused;
+
+	public void returnToMainMenu () {
+        print ("returning to main menu");
+        SceneManager.LoadScene ("mainmenu");
     }
+
+    public void OnPauseGame () {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0f : 1f;
+        //pauseMenuCanvas.SetActive (isPaused);
+	}
 }
